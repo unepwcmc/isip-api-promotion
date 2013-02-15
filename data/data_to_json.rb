19d9152cc576a23f625b9424c654990bbb2e6407
Simao Belchior de Castro <simaobelchior@gmail.com>
2013-02-15 15:00:20 +0000

    Fixed small issues with json generation. moved to appending both files together

--- data/data_to_json.rb
+++ data/data_to_json.rb
@@ -5,6 +5,8 @@ f_animals = 'animals_utf8.csv'
 f_plants = 'plants_utf8.csv'
 
 change_id = 1
+species = []
+changes = []
 
 [f_animals, f_plants].each do |file|
   puts "Opening file: #{file}"
@@ -16,13 +18,11 @@ change_id = 1
   # 6: current_listing
   # 10: change_type
   # 11: relevant_listing
-  species_json = []
-  changes_json = []
   ids = []
   table[1, table.size-1].each do |row|
     unless ids.include? row[4]
       ids << row[4]
-      species_json << {
+      species << {
         :id => row[4],
         :full_name => row[1],
         :author_year => row[2],
@@ -31,20 +31,21 @@ change_id = 1
         :relevant_listing => row[11]
       }
     end
-    changes_json << {
+    changes << {
       :id => change_id,
       :species_id => row[4],
       :change_type => row[10],
       :species_listing => row[11]
     }
-  end
-  puts "Adding data to species.json"
-  File.open("species.json", "a") do |f|
-    f.write(species_json.to_json)
-  end
-  puts "Adding data to changes.json"
-  File.open("changes.json", "a") do |f|
-    f.write(changes_json.to_json)
+    change_id += 1
   end
   puts "Finished file #{file}"
+end
+puts "Adding data to species.json"
+File.open("species.json", "a") do |f|
+  f.write(species.to_json)
+end
+puts "Adding data to changes.json"
+File.open("changes.json", "a") do |f|
+  f.write(changes.to_json)
 end
